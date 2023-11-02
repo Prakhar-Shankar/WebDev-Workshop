009d275d9526603cd673b2237847989d0048abba
Prexy <97254881+Prakhar-Shankar@users.noreply.github.com>
2023-11-03 02:33:11 +0530

    Update cardList.tsx

--- components/cardList.tsx
+++ components/cardList.tsx
@@ -4,7 +4,7 @@ import cardData from './cardData'; // Import the data source
 
 function CardList() {
   return (
-    <div className="ml-16 mt-[10rem] flex gap-10">
+    <div className="ml-16 mt-[10rem] gap-10 flex flex-wrap">
       {cardData.map((card, index, link) => (
         <Card key={index} title={card.name} description={card.favDomain} link={card.link}/>
       ))}
