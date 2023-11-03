fc580187e33b6179a313cf59b7c4c3d01ae5a283
prachi0625 <147588772+prachi0625@users.noreply.github.com>
2023-11-03 14:07:36 +0530

    Update cardData.js

--- components/cardData.js
+++ components/cardData.js
@@ -43,37 +43,9 @@ const cardData = [
     //   link: "GITHUB LINK "
       
     // },
-const cardData = [
-    {
-      name: "Prakhar Shankar",
-      favDomain: "Web development",
-      link: "https://github.com/Prakhar-Shankar"
-      
-    },
-    {
-      name: "Pradyot Ranjan",
-      favDomain: "Dev-ops",
-      link: "https://github.com/prady0t"
-    },
-    {
-        name: "Arnav Bhargav",
-        favDomain: "Operating System",
-        link: "https://github.com/SOURHEAD"
-      },
-    {
-        name: "t.e_.n._e.t",
-        favDomain: "Web3",
-        link: "https://github.com/mrswastik-robot"
-    },
-    
-    // Add more card objects here
-  ];
-  
-  export default cardData;
+
   
 
-    // Add more card objects here
-  ];
   
   export default cardData;
   
