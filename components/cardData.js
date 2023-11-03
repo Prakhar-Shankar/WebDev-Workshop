48dea027f9f2ddd647faa5a963772c39ce5405bb
prachi0625 <147588772+prachi0625@users.noreply.github.com>
2023-11-03 14:06:03 +0530

    Update cardData.js

--- components/cardData.js
+++ components/cardData.js
@@ -29,7 +29,49 @@ const cardData = [
         favDomain: "Web3",
         link: "https://github.com/mrswastik-robot"
     },
+    {
+        name: "prachi0625",
+        favDomain: "Webdevelopment",
+        link: "https://github.com/prachi0625/WebDev-Workshop/edit/main/components/cardData.js"
+    },
+    // cardData.js
+
+//Use this template to mark your attendence.
+    // {
+    //   name: "YOUR NAME",
+    //   favDomain: "WHAT ARE YOUR INTRESTS",
+    //   link: "GITHUB LINK "
+      
+    // },
+const cardData = [
+    {
+      name: "Prakhar Shankar",
+      favDomain: "Web development",
+      link: "https://github.com/Prakhar-Shankar"
+      
+    },
+    {
+      name: "Pradyot Ranjan",
+      favDomain: "Dev-ops",
+      link: "https://github.com/prady0t"
+    },
+    {
+        name: "Arnav Bhargav",
+        favDomain: "Operating System",
+        link: "https://github.com/SOURHEAD"
+      },
+    {
+        name: "t.e_.n._e.t",
+        favDomain: "Web3",
+        link: "https://github.com/mrswastik-robot"
+    },
     
+    // Add more card objects here
+  ];
+  
+  export default cardData;
+  
+
     // Add more card objects here
   ];
   
